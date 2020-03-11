9ec3a2366b02021677d97dd690d250750f69a4d2
alex-steinberg <steinberg.alex@gmail.com>
2020-03-11 13:03:53 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Ionic React with SQLite
+
+Quick example of using SQLite with Ionic React. This was created  
+using the blank Ionic starter app and the sample code on the [SQLite plugin docs](https://ionicframework.com/docs/native/sqlite).
+
+I've only used it with Android but iOS should work all the same.
+
+## Installation
+
+Clone this repo then:
+
+`npm install`
+
+`ionic cap add android`
+
+`ionic cap sync`
+
+`ionic cap open android`
+
+Open Android Studio, plug in your device and run the app.
+
+Tap the 'INIT DB' button.
+
+Open Chrome, go to chrome://inspect and inspect the app to view the results in the JavaScript console.
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,25 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 const Home: React.FC = () => {
+  const initDb = (): void => {
+      console.log('initDB fired!');
+      SQLite.create({
+          name: 'data.db', location: 'default'
+      }).then(async (db: SQLiteObject) => {
+          try {
+              const create = await db.executeSql('create table if not exists danceMoves(name VARCHAR(32))', []);
+              await console.log('Table created/exists. Msg: ', create);
+              const insert = await db.executeSql('insert into danceMoves (name) values (?)', ['Macarena']);
+              await console.log('Inserted Macarena: ', insert);
+          } catch (e) {
+              console.log('SQL error: ', e);
+          }
+      })
+  };
   return (
     <IonPage>
       <IonHeader>
@@ -17,6 +33,7 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
+          <IonButton onClick={initDb}>Init DB</IonButton>
         <ExploreContainer />
       </IonContent>
     </IonPage>
