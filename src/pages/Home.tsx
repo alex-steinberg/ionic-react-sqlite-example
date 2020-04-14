37cfb7da3617abf13b8178c92d1bbd4f79a82a08
alex-steinberg <steinberg.alex@gmail.com>
2020-04-14 12:00:19 +0200

    alert to explain that this works only on a device

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,31 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonAlert } from '@ionic/react';
+import React, { useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 const Home: React.FC = () => {
+  const [showAlert, setShowAlert] = useState(false);
   const initDb = (): void => {
       console.log('initDB fired!');
-      SQLite.create({
+      try {
+        SQLite.create({
           name: 'data.db', location: 'default'
-      }).then(async (db: SQLiteObject) => {
-          try {
+        }).then(async (db: SQLiteObject) => {
+            try {
               const create = await db.executeSql('create table if not exists danceMoves(name VARCHAR(32))', []);
               await console.log('Table created/exists. Msg: ', create);
               const insert = await db.executeSql('insert into danceMoves (name) values (?)', ['Macarena']);
               await console.log('Inserted Macarena: ', insert);
-          } catch (e) {
+            } catch (e) {
               console.log('SQL error: ', e);
-          }
-      })
+            }
+        })
+      } catch(e) {
+        setShowAlert(true);
+        console.log('please use a device: ', e)
+      }
+      
   };
   return (
     <IonPage>
@@ -34,6 +41,13 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
           <IonButton onClick={initDb}>Init DB</IonButton>
+          <IonAlert
+            isOpen={showAlert}
+            onDidDismiss={() => setShowAlert(false)}
+            header={'Mamma mia!'}
+            message={'This will only work on a device. Please refer to the README.'}
+            buttons={['OK']}
+          />
         <ExploreContainer />
       </IonContent>
     </IonPage>
